76fbdd54fa9dea38e4e21c72be3da6f8d3e5bb35
DianLesmana08 <lesmanadian120@gmail.com>
2021-08-31 21:47:34 +0700

    Session 3 and package

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def add(*args):
+    res=0
+    for arg in args:
+        res+=arg
+    return res
+def multiply(*args):
+    res=1
+    for arg in args:
+        res=res*arg
+    return res
+    
+
+
+# In[ ]:
+
+
+
+
